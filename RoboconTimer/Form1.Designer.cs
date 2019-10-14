2fd46c55d592153fd51a4bb4dadb403bda56a92e
garyo <surpace2479@gmail.com>
2019-10-15 03:04:33 +0900

    init

---
+++ RoboconTimer/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace RoboconTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnSetting = new System.Windows.Forms.Button();
+            this.btnWhistle = new System.Windows.Forms.Button();
+            this.lblSetting = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txbGameTime = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txbSettingTime = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("DSEG14 Modern", 71.99999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(3, 3);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(628, 326);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "READY";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 30;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 12;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Font = new System.Drawing.Font("Meiryo UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnStart.Location = new System.Drawing.Point(504, 286);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(122, 38);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Start(s)";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnReset.Font = new System.Drawing.Font("Meiryo UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnReset.Location = new System.Drawing.Point(120, 286);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(122, 38);
+            this.btnReset.TabIndex = 7;
+            this.btnReset.Text = "Reset(r)";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnSetting
+            // 
+            this.btnSetting.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSetting.Font = new System.Drawing.Font("Meiryo UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnSetting.Location = new System.Drawing.Point(376, 286);
+            this.btnSetting.Name = "btnSetting";
+            this.btnSetting.Size = new System.Drawing.Size(122, 38);
+            this.btnSetting.TabIndex = 8;
+            this.btnSetting.Text = "Setting(c)";
+            this.btnSetting.UseVisualStyleBackColor = true;
+            this.btnSetting.Click += new System.EventHandler(this.btnSetting_Click);
+            // 
+            // btnWhistle
+            // 
+            this.btnWhistle.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnWhistle.Font = new System.Drawing.Font("Meiryo UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnWhistle.Location = new System.Drawing.Point(248, 286);
+            this.btnWhistle.Name = "btnWhistle";
+            this.btnWhistle.Size = new System.Drawing.Size(122, 38);
+            this.btnWhistle.TabIndex = 11;
+            this.btnWhistle.Text = "Whistle(w)";
+            this.btnWhistle.UseVisualStyleBackColor = true;
+            this.btnWhistle.Click += new System.EventHandler(this.btnWhistle_Click);
+            // 
+            // lblSetting
+            // 
+            this.lblSetting.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblSetting.AutoSize = true;
+            this.lblSetting.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblSetting.ForeColor = System.Drawing.Color.White;
+            this.lblSetting.Location = new System.Drawing.Point(556, 258);
+            this.lblSetting.Name = "lblSetting";
+            this.lblSetting.Size = new System.Drawing.Size(70, 25);
+            this.lblSetting.TabIndex = 14;
+            this.lblSetting.Text = "label4";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Font = new System.Drawing.Font("Meiryo UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(642, 362);
+            this.tabControl1.TabIndex = 15;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.DimGray;
+            this.tabPage1.Controls.Add(this.lblSetting);
+            this.tabPage1.Controls.Add(this.btnStart);
+            this.tabPage1.Controls.Add(this.btnSetting);
+            this.tabPage1.Controls.Add(this.btnWhistle);
+            this.tabPage1.Controls.Add(this.btnReset);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 26);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(634, 332);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "カウンター";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.White;
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.txbSettingTime);
+            this.tabPage2.Controls.Add(this.label7);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.txbGameTime);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Location = new System.Drawing.Point(4, 26);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(634, 332);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "設定";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 18);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "試合時間";
+            // 
+            // txbGameTime
+            // 
+            this.txbGameTime.Location = new System.Drawing.Point(112, 12);
+            this.txbGameTime.Name = "txbGameTime";
+            this.txbGameTime.Size = new System.Drawing.Size(100, 24);
+            this.txbGameTime.TabIndex = 1;
+            this.txbGameTime.Text = "150";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(218, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(22, 18);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "秒";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(218, 45);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(22, 18);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "秒";
+            // 
+            // txbSettingTime
+            // 
+            this.txbSettingTime.Location = new System.Drawing.Point(112, 42);
+            this.txbSettingTime.Name = "txbSettingTime";
+            this.txbSettingTime.Size = new System.Drawing.Size(100, 24);
+            this.txbSettingTime.TabIndex = 4;
+            this.txbSettingTime.Text = "60";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(8, 45);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(98, 18);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "セッティングタイム";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(642, 362);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.DoubleBuffered = true;
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnSetting;
+        private System.Windows.Forms.Button btnWhistle;
+        private System.Windows.Forms.Label lblSetting;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txbSettingTime;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txbGameTime;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ RoboconTimer/Form1.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RoboconTimer
+{
+    public partial class Form1 : Form
+    {
+        DateTime start_time;
+        int timer_state = 0; // 0: 初期状態
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        int text_size;
+        string display_str;
+        int text_size_1 = 0;
+        int text_size_2 = 0;
+        bool sound_flag = false;
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // フォームの幅を取得
+            int w = this.Width;
+            text_size_1 = (int)(w * 0.15);
+            text_size_2 = (int)(text_size_1 * 1.2);
+            
+            int game_time = int.Parse(txbGameTime.Text);
+            int setting_time = int.Parse(txbSettingTime.Text);
+
+            // 現在時間の取得
+            DateTime now_time = DateTime.Now;
+
+
+            lblSetting.Text = "";
+
+
+            // カウント待機
+            if (timer_state == 0)
+            {
+                display_str = "READY";
+                text_size = text_size_1;
+            }
+
+            // スタートカウントダウン
+            if (timer_state == 1)
+            {
+                long elapsed_sec = GetUnixTime(now_time) - GetUnixTime(start_time);
+                int remaining_sec = 6 - (int)elapsed_sec;
+
+                if (remaining_sec != 6)
+                {
+                    display_str = (remaining_sec).ToString();
+                    text_size = text_size_2;
+                }
+
+                if (remaining_sec == 3 && sound_flag == false)
+                {
+                    sound_flag = true;
+                    PlaySound("count");
+                }
+
+                if (remaining_sec == 0)
+                {
+                    timer_state++;
+                }
+            }
+
+            // 「START」描画
+            if (timer_state == 2)
+            {
+                sound_flag = false;
+
+                long elapsed_sec = GetUnixTime(now_time) - GetUnixTime(start_time) - 6;
+
+                display_str = "START";
+                text_size = text_size_1;
+
+                if (elapsed_sec > 1)
+                {
+                    timer_state++;
+                }
+            }
+
+            // 経過時間の表示
+            if (timer_state == 3)
+            {
+                long elapsed_sec = GetUnixTime(now_time) - GetUnixTime(start_time) - 6;
+                int remaining_sec = 6 - (int)elapsed_sec;
+
+                // 描画文字列の生成
+                string min_str = (elapsed_sec / 60).ToString();
+                string sec_str = (elapsed_sec % 60).ToString();
+
+                // ゼロ埋め
+                if (elapsed_sec % 60 < 10)
+                {
+                    sec_str = "0" + sec_str;
+                }
+
+                // 終了カウントダウン
+                if (elapsed_sec >= game_time - 3)
+                {
+                    label1.ForeColor = Color.Yellow;
+                    if (sound_flag == false)
+                    {
+                        sound_flag = true;
+                        PlaySound("count");
+                    }
+                }
+
+                // 試合時間終了後は赤
+                if (elapsed_sec >= game_time)
+                {
+                    label1.ForeColor = Color.Red;
+                }
+
+                // 10分以上はカラム落ちするので、リセット
+                if (elapsed_sec >= 600)
+                {
+                    reset();
+                }
+
+                display_str = min_str + ":" + sec_str;
+                text_size = text_size_2;
+            }
+
+            // セッティングタイムカウント
+            if (timer_state == 10)
+            {
+                long elapsed_sec = GetUnixTime(now_time) - GetUnixTime(start_time);
+
+                lblSetting.Text = elapsed_sec.ToString();
+                if (elapsed_sec == setting_time)
+                {
+                    PlaySound("whistle");
+                    timer_state = 11;
+                }
+            }
+
+            label1.Text = display_str;
+            label1.Font = new System.Drawing.Font("DSEG14 Modern", text_size);
+        }
+
+        private void start()
+        {
+            PlaySound("startcall");
+
+            // スタート時間を記録
+            start_time = DateTime.Now;
+            timer_state = 1;
+        }
+
+        private void setting()
+        {
+            PlaySound("whistle");
+
+            // スタート時間を記録
+            start_time = DateTime.Now;
+            timer_state = 10;
+        }
+
+        private void whistle()
+        {
+            PlaySound("whistle");
+        }
+
+        private void reset()
+        {
+            timer_state = 0; 
+            sound_flag = false;
+            label1.ForeColor = Color.White;
+            label1.Text = "READY";
+        }
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode.ToString() == "s" || e.KeyCode.ToString() == "S")
+            {
+                start();
+            }
+
+            if (e.KeyCode.ToString() == "c" || e.KeyCode.ToString() == "C")
+            {
+                setting();
+            }
+
+            if (e.KeyCode.ToString() == "w" || e.KeyCode.ToString() == "W")
+            {
+                whistle();
+            }
+
+            if (e.KeyCode.ToString() == "r" || e.KeyCode.ToString() == "R")
+            {
+                reset();
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            reset();
+        }
+
+
+        private void btnSetting_Click(object sender, EventArgs e)
+        {
+            setting();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            start();
+        }
+
+        private void btnWhistle_Click(object sender, EventArgs e)
+        {
+            whistle();
+        }
+
+
+
+
+
+
+
+
+
+
+        private System.Media.SoundPlayer player = null;
+        //WAVEファイルを再生する
+        private void PlaySound(string waveFileName)
+        {
+            //再生されているときは止める
+            if (player != null)
+                StopSound();
+
+            //読み込む
+            player = new System.Media.SoundPlayer(System.IO.Path.GetFullPath(".\\sound\\" + waveFileName + ".wav"));
+            //非同期再生する
+            player.Play();
+
+            //次のようにすると、ループ再生される
+            //player.PlayLooping();
+
+            //次のようにすると、最後まで再生し終えるまで待機する
+            //player.PlaySync();
+        }
+
+        //再生されている音を止める
+        private void StopSound()
+        {
+            if (player != null)
+            {
+                player.Stop();
+                player.Dispose();
+                player = null;
+            }
+        }
+
+
+        // UNIXエポックを表すDateTimeオブジェクトを取得
+        private static DateTime UNIX_EPOCH = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+        public static long GetUnixTime(DateTime targetTime)
+        {
+            // UTC時間に変換
+            targetTime = targetTime.ToUniversalTime();
+
+            // UNIXエポックからの経過時間を取得
+            TimeSpan elapsedTime = targetTime - UNIX_EPOCH;
+
+            // 経過秒数に変換
+            return (long)elapsedTime.TotalSeconds;
+        }
+    }
+}
+
+
+
+
